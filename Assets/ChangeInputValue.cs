2cbd85050f10f7fd930fdfab3fe9aef2efe3a7ba
Sérgio Macellani <sergiomacellani@sergiom.dev>
2022-11-02 15:04:13 -0300

    value input

--- Assets/ChangeInputValue.cs
+++ Assets/ChangeInputValue.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeInputValue : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI valueText;
+    [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private TMP_InputField valueText;
+    [SerializeField] private Image[] modifierImages;
+    
+    private Color _activeColor = new Color(1,1,1,.25f);
+    private Color _inactiveColor = new Color(.35f,.35f,.35f,.25f);
+    
     private float value;
     private bool isFloat = true;
-    private float[] modiferValues = { .25f, 1, 10 };
+    private float[] modiferValues = { .25f, .5f, 1, 5, 10 };
+    private float valueModifier = 1;
+    
+    private InputFieldLagFix _input;
+    private CanvasGroup _canvasGroup => GetComponent<CanvasGroup>();
+
+    public void OpenChangeInput(InputFieldLagFix input, bool isFloat = true, string title = "")
+    {
+        _input = input;
+        this.isFloat = isFloat;
+        CanvasGroupSettings(true);
+        valueText.text = input.GetInputText;
+        value = float.Parse(input.GetInputText);
+        ChangeModifier(2);
+        titleText.text = $"MODIFICAR VALOR{(title != "" ? $" DE {title}" : $"")}";
+        
+        modifierImages[0].GetComponent<Button>().interactable = isFloat;
+        modifierImages[1].GetComponent<Button>().interactable = isFloat;
+    }
     
     public void ChangeValue(float val)
     {
-        value = val;
-        valueText.text = value.ToString(isFloat ? "F2" : "F0");
+        value += val * valueModifier;
+        valueText.text = value.ToString(isFloat ? "0.##" : "F0");
+    }
+    
+    public void ChangeValue(string val)
+    {
+        value = float.Parse(val);
+        valueText.text = value.ToString(isFloat ? "0.##" : "F0");
+    }
+    
+    public void ChangeModifier(int index)
+    {
+        for (int i = 0; i < modifierImages.Length; i++)
+        {
+            modifierImages[i].color = i == index ? _activeColor : _inactiveColor;
+        }
+    
+        valueModifier = modiferValues[index];
+    }
+    
+    public void SaveValue()
+    {
+        _input.SaveValue(valueText.text);
+        CanvasGroupSettings(false);
+        _input = null;
+    }
+
+    public void CancelChange()
+    {
+        CanvasGroupSettings(false);
+        _input = null;
+    }
+    
+    private void CanvasGroupSettings(bool active)
+    {
+        _canvasGroup.alpha = active ? 1 : 0;
+        _canvasGroup.interactable = active;
+        _canvasGroup.blocksRaycasts = active;
     }
 }
--- Assets/InputFieldLagFix.cs
+++ Assets/InputFieldLagFix.cs
@@ -5,22 +5,45 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InputFieldLagFix : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class InputFieldLagFix : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    [SerializeField]
+    private bool isString = false;
+    [SerializeField]
+    private bool isFloat = true;
+    [SerializeField]
+    private string title = "";
     private TMP_InputField _inputField;
+    
+    public string GetInputText => _inputField.text;
+    
     public void Start()
     {
         TryGetComponent(out _inputField);
         _inputField.enabled = false;
     }
 
+    public void SaveValue(string value)
+    {
+        _inputField.text = value;
+        _inputField.onEndEdit.Invoke(value);
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        _inputField.enabled = true;
+        if(isString)
+            _inputField.enabled = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        _inputField.enabled = false;
+        if(isString)
+            _inputField.enabled = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(!isString)
+            GameObject.FindWithTag("ChangeValue").GetComponent<ChangeInputValue>().OpenChangeInput(this, isFloat, title);
     }
 }
